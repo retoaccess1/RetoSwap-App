ae00ade2eaaffb40ad9bd0139d653d98eccc0d1f
atsamd21 <atsamd21@proton.me>
2025-08-14 20:04:52 +0200

    Add preferences helper

---
+++ Helpers/Preferences.cs
@@ -0,0 +1,54 @@
+﻿#if ANDROID
+using Android.Content;
+using System.Text.Json;
+#endif
+
+namespace Manta.Helpers;
+
+public static class Preferences
+{
+    public const string SeedBackupDone = "seed-backup-done";
+    //
+    public const string SelectedCurrencyCode = "selected-currency-code";
+    public const string SelectedPaymentMethod = "selected-payment-method";
+    public const string ShowNoDepositOffers = "show-no-deposit-offers";
+    public const string IsToggled = "is-toggled";
+    public const string OfferPaymentType = "offer-payment-type";
+
+#if ANDROID
+    public static void Set<T>(string key, T? data)
+    {
+        var sharedPreferences = Android.App.Application.Context.GetSharedPreferences("data", FileCreationMode.Private);
+        var editor = sharedPreferences?.Edit();
+
+        editor?.PutString(key, JsonSerializer.Serialize(data));
+        editor?.Apply();
+    }
+
+    public static T? Get<T>(string key)
+    {
+        try
+        {
+            var str = Android.App.Application.Context.GetSharedPreferences("data", FileCreationMode.Private)?.GetString(key, null);
+            if (str is null)
+                return default;
+
+            return JsonSerializer.Deserialize<T>(str);
+        }
+        catch
+        {
+            return default;
+        }
+    }
+#else
+    public static void Set<T>(string key, T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static T? Get<T>(string key)
+    {
+        throw new NotImplementedException();
+    }
+#endif
+}
